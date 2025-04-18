28061dffe56cfb687214f56a93b6c6a4f8787ff7
Luis Augusto <luisaugusto010203@gmail.com>
2025-04-17 22:39:54 -0300

    Projeto criado

---
+++ tetris-console/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace tetris_console
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
