c16be366983799ff9f75758477d3729de0cf1432
Luis Augusto <luisaugusto010203@gmail.com>
2025-04-21 11:45:19 -0300

    Adicionando a classe game e pegando input do teclado

---
+++ tetris-console/Entities/Game.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Threading;
+
+namespace Entities {
+    class Game {
+        private Piece current;
+        private Grid grid;
+
+        public Game(Grid grid) {
+            this.grid = grid;
+            current = current.RandomBlock();
+        }
+
+        public void InputHandler() {
+            while (Console.KeyAvailable) {
+                ConsoleKey key = Console.ReadKey(true).Key;
+
+                switch (key) {
+                    case ConsoleKey.LeftArrow:
+                        grid.MoveLeft(current);
+                        break;
+                    case ConsoleKey.RightArrow:
+                        grid.MoveRight(current);
+                        break;
+                    case ConsoleKey.DownArrow:
+                        grid.MoveDown(current);
+                        break;
+                    case ConsoleKey.UpArrow:
+                        current.Rotate();
+                        if (!grid.IsValidPosition(current)) {
+                            current.Rotate();
+                            current.Rotate();
+                            current.Rotate();
+                        }
+                        break;
+                }
+            }
+        }
+    }
+}
